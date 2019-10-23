a54bc0dd732b3bba5249e56d4409c4396a724cce
fivge <luanxingtong@gmail.com>
2019-10-23 16:18:42 +0800

    toh4
    You created a service class that can be shared by many components.
    You used the ngOnInit lifecycle hook to get the hero data when the AppComponent activates.
    You defined the HeroService as a provider for the AppComponent.
    You designed the service to return a Future and the component to get the data from the Future.

--- lib/app_component.dart
+++ lib/app_component.dart
@@ -2,21 +2,39 @@ import 'package:angular/angular.dart';
 
 import 'src/hero.dart';
 import 'src/hero_component.dart';
-import 'src/mock_heroes.dart';
+import 'src/hero_service.dart';
 
 @Component(
   selector: 'my-app',
   templateUrl: './app_component.html',
   styleUrls: ['./app_component.css'],
   directives: [coreDirectives, HeroComponent],
+  providers: [ClassProvider(HeroService)],
 )
-class AppComponent {
+class AppComponent implements OnInit {
   final title = 'Tour of Heroes';
 
-  // Hero hero = Hero(52788, 'Windstorm');
-  List<Hero> heroes = mockHeroes;
+  List<Hero> heroes;
 
   Hero selected;
+
+  final HeroService _heroService;
+
+  AppComponent(this._heroService);
+
+  void ngOnInit() => _getHeroes();
+  // void ngOnInit() {
+  //   _getHeroes();
+  // }
+
+  // void _getHeroes() {
+  //   _heroService.getAll().then((heroes) => this.heroes = heroes);
+  // }
+
+  Future<void> _getHeroes() async {
+    heroes = await _heroService.getAll();
+    // heroes = await _heroService.getAllSlowly();
+  }
 
   void onSelect(Hero hero) => selected = hero;
 }
---
+++ lib/src/hero_service.dart
@@ -0,0 +1,14 @@
+import 'hero.dart';
+import 'mock_heroes.dart';
+
+class HeroService {
+  Future<List<Hero>> getAll() async => mockHeroes;
+
+  Future<List<Hero>> getAllSlowly() {
+    return Future.delayed(Duration(seconds: 2), getAll);
+  }
+
+  Future<List<Hero>> getHeroes() async {
+    return mockHeroes;
+  }
+}
