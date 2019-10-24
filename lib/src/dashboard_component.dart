50762c448295a43b012dba8e5f02bce4eccd26bc
fivge <luanxingtong@gmail.com>
2019-10-24 17:17:59 +0800

    toh6
    You added the necessary dependencies to use HTTP in the app.
    You refactored HeroService to load heroes from a web API.
    You extended HeroService to support post(), put(), and delete() methods.
    You updated the components to allow adding, editing, and deleting of heroes.
    You configured an in-memory web API.
    You learned how to use Streams.

--- lib/src/dashboard_component.dart
+++ lib/src/dashboard_component.dart
@@ -2,14 +2,15 @@ import 'package:angular/angular.dart';
 import 'package:angular_router/angular_router.dart';
 
 import 'routes.dart';
-import 'hero.dart';
 import 'hero_service.dart';
+import 'hero.dart';
+import 'hero_search_component.dart';
 
 @Component(
   selector: 'my-dashboard',
   templateUrl: './dashboard_component.html',
   styleUrls: ['./dashboard_component.css'],
-  directives: [coreDirectives, routerDirectives],
+  directives: [coreDirectives, routerDirectives, HeroSearchComponent],
 )
 class DashboardComponent implements OnInit {
   List<Hero> heroes;
